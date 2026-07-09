d1fa6d84a307eb33795589880279fe7d8565baa5
bashbunni <bunni@bashbunni.dev>
2026-07-08 18:36:20 -0700

    feat: simplify nix flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "A basic Elm project";
+  description = "A simple elm project";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,58 +12,30 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages.default =
-          let
-            npmDeps = import ./npmDeps.nix;
-          in
-          pkgs.buildNpmPackage {
-            name = "frontend";
-            src = ./.;
-
-            npmBuildScript = "build";
-
-            inherit (npmDeps) npmDepsHash;
-
-            doCheck = true;
-
-            nativeBuildInputs = [
-              pkgs.elmPackages.elm
-              pkgs.elmPackages.elm-test
-            ];
-
-            configurePhase =
-              let
-                elmConfigure =
-                  pkgs.elmPackages.fetchElmDeps {
-                    elmPackages = import ./elm-srcs.nix;
-                    elmVersion = "0.19.1";
-                    registryDat = ./registry.dat;
-                  };
-              in
-              ''
-                ${elmConfigure}
-              '';
-
-            checkPhase = ''
-              elm-test
-            '';
-
-            installPhase = ''
-              mkdir -p $out/dist
-              mv dist/* $out/dist
-            '';
-          };
-
         devShells.default = pkgs.mkShell {
-          name = "elm2nix-example";
+          name = "elm-integer";
 
           packages = with pkgs.elmPackages; [
             elm
+            elm-doc-preview
+            elm-language-server
+            elm-format
+            elm-optimize-level-2
             elm-test
-            pkgs.elm2nix
+            pkgs.caddy
             pkgs.nodejs_20
-            pkgs.prefetch-npm-deps
+            pkgs.nodePackages.terser
+            pkgs.shellcheck
           ];
+
+          shellHook =
+            ''
+            export project="$PWD"
+            export build="$project/.build"
+            export PATH="$project/bin:$PATH"
+
+            npm install --loglevel silent
+            '';
         };
       }
     );
