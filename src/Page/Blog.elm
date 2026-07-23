e6a003421ea5dfc92b22f557545e3678290398a0
bashbunni <bunni@bashbunni.dev>
2026-07-23 16:06:24 -0700

    feat: print errors

--- src/Page/Blog.elm
+++ src/Page/Blog.elm
@@ -8,53 +8,45 @@ import Json.Decode exposing (Decoder, field, int, list, map3, map5, string)
 
 type State
     = Loading
-    | ErrView string
-    | Loaded
+    | ErrView Http.Error
+    | Loaded Blog
 
 
 type alias Model =
-    { blog : Blog
-    , state : State
-    }
+    State
 
 
 displayPosts : Model -> Html Msg
 displayPosts model =
-    ul []
-        (model.blog.items
-            |> List.map
-                (\item ->
-                    li []
-                        [ a
-                            [ class "blog-link"
-                            , href item.url
-                            , alt item.title
-                            ]
-                            [ text item.title ]
-                        ]
+    case model of
+        Loaded blog ->
+            ul []
+                (blog.items
+                    |> List.map
+                        (\item ->
+                            li []
+                                [ a
+                                    [ class "blog-link"
+                                    , href item.url
+                                    , alt item.title
+                                    ]
+                                    [ text item.title ]
+                                ]
+                        )
                 )
-        )
+
+        _ ->
+            div [] [ text "nothing yet!" ]
 
 
 type Msg
     = GotBlog (Result Http.Error Blog)
 
 
-empty : Model
-empty =
-    { blog =
-        { title = ""
-        , author = ""
-        , items = []
-        }
-    , state = Loading
-    }
-
-
 init : ( Model, Cmd Msg )
 init =
     -- command fetches + decodes blog posts
-    ( empty
+    ( Loading
     , Http.get
         { url = "/posts.json"
         , expect = Http.expectJson GotBlog blogDecoder
@@ -63,19 +55,47 @@ init =
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
+update msg _ =
     case msg of
         GotBlog (Ok result) ->
             -- do something if everything loaded
-            ( { model | blog = result }, Cmd.none )
+            ( Loaded result, Cmd.none )
 
         GotBlog (Err result) ->
-            ( { model | state = ErrView result }, Cmd.none )
+            ( ErrView result, Cmd.none )
 
 
 view : Model -> Html Msg
-view _ =
-    div [] [ text "welcome to my blog" ]
+view model =
+    case model of
+        Loading ->
+            div [] [ text "loading" ]
+
+        Loaded _ ->
+            -- TODO just pass blog, not the whole model
+            displayPosts model
+
+        ErrView err ->
+            div [] [ text ("ran into an error :(" ++ errorToString err) ]
+
+
+errorToString : Http.Error -> String
+errorToString error =
+    case error of
+        Http.NetworkError ->
+            "Network error"
+
+        Http.Timeout ->
+            "Timed out"
+
+        Http.BadUrl url ->
+            "Bad URL: " ++ url
+
+        Http.BadBody body ->
+            "Bad BODY: " ++ body
+
+        Http.BadStatus status ->
+            "Bad STATUS: " ++ String.fromInt status
 
 
 type alias Blog =
@@ -85,15 +105,11 @@ type alias Blog =
     }
 
 
-
--- blogDecoder: decode json into Blog type
-
-
 blogDecoder : Decoder Blog
 blogDecoder =
     map3 Blog
         (field "title" string)
-        (field "title" string)
+        (field "author" string)
         (field "items" (list postDecoder))
 
 
@@ -122,4 +138,4 @@ postDecoder =
 -- need loading state?
 -- TODO figure out how to init THIS model; does it start empty, then fetch posts from
 -- init, to handle in update?
---
+-- TODO centralize state to hold blog; you can't have blog when it's loading anyway
